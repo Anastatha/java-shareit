9fdaa11c053ca4f1e5e4b1f1f89ddf271721a34b
RodionovaAP <rodionovaap@admlr.lipetsk.ru>
2026-05-11 16:58:55 +0300

    feat: @UtilityClass in mapper

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -1,40 +1,39 @@
 package ru.practicum.shareit.item;
 
+import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.request.ItemRequest;
 
-public final class ItemMapper {
+@UtilityClass
+public class ItemMapper {
 
-	private ItemMapper() {
-	}
+    public static ItemDto toItemDto(Item item) {
+        if (item == null) {
+            return null;
+        }
+        return new ItemDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.isAvailable(),
+                item.getRequest() != null ? item.getRequest().getId() : null
+        );
+    }
 
-	public static ItemDto toItemDto(Item item) {
-		if (item == null) {
-			return null;
-		}
-		return new ItemDto(
-			item.getId(),
-			item.getName(),
-			item.getDescription(),
-			item.isAvailable(),
-			item.getRequest() != null ? item.getRequest().getId() : null
-		);
-	}
-
-	public static Item toItem(ItemDto itemDto) {
-		if (itemDto == null) {
-			return null;
-		}
-		Item item = new Item();
-		item.setId(itemDto.getId());
-		item.setName(itemDto.getName());
-		item.setDescription(itemDto.getDescription());
-		if (itemDto.getAvailable() != null) {
-			item.setAvailable(itemDto.getAvailable());
-		}
-		if (itemDto.getRequestId() != null) {
-			item.setRequest(new ItemRequest(itemDto.getRequestId(), null, null, null));
-		}
-		return item;
-	}
+    public static Item toItem(ItemDto itemDto) {
+        if (itemDto == null) {
+            return null;
+        }
+        Item item = new Item();
+        item.setId(itemDto.getId());
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        if (itemDto.getAvailable() != null) {
+            item.setAvailable(itemDto.getAvailable());
+        }
+        if (itemDto.getRequestId() != null) {
+            item.setRequest(new ItemRequest(itemDto.getRequestId(), null, null, null));
+        }
+        return item;
+    }
 }
--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -1,23 +1,22 @@
 package ru.practicum.shareit.user;
 
+import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.user.dto.UserDto;
 
-public final class UserMapper {
+@UtilityClass
+public class UserMapper {
 
-	private UserMapper() {
-	}
+    public static UserDto toUserDto(User user) {
+        if (user == null) {
+            return null;
+        }
+        return new UserDto(user.getId(), user.getName(), user.getEmail());
+    }
 
-	public static UserDto toUserDto(User user) {
-		if (user == null) {
-			return null;
-		}
-		return new UserDto(user.getId(), user.getName(), user.getEmail());
-	}
-
-	public static User toUser(UserDto userDto) {
-		if (userDto == null) {
-			return null;
-		}
-		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
-	}
+    public static User toUser(UserDto userDto) {
+        if (userDto == null) {
+            return null;
+        }
+        return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
+    }
 }
